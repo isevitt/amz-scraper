01615771093a4ab29e03c4ac9df02327ab5a3c7e
Itai Sevitt <isevitt@gmail.com>
2020-03-16 16:12:05 +0200

    first

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib3
+import re
+
+BASE_URL = "https://www.amazon.com/s?i=merchant-items&me="
+
+def get_products_by_store(merchant_id):
+    pass
+
+if __name__ == "__main__":
+    get_products_by_store("AK3PDFUAKQTEL")
