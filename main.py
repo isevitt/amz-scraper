0d99c22982f4fa9bd7808ad0b254d1516bbf02b6
Itai Sevitt <isevitt@gmail.com>
2020-03-16 19:17:29 +0200

    selenium

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from selenium import webdriver
 import os
 from configs import local
 
-BASE_URL = "https://www.amazon.com/s?i=merchant-items&me="
+SELLER_BASE_URL = "https://www.amazon.com/s?i=merchant-items&me="
+PROD_BASE_URL = "https://www.amazon.com/dp/"
+NUM_PAGES = 1
 CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
-
 
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-gpu")
@@ -17,18 +18,24 @@ if not local:
     options.add_argument('headless')
 
 
-
-
 def get_product_list(merchant_id, pages_num):
     all_asins = []
-    for page in range(1, pages_num+1):
-        driver.get(f"{BASE_URL}{merchant_id}&page={page}")
+    for page in range(1, pages_num + 1):
+        driver.get(f"{SELLER_BASE_URL}{merchant_id}&page={page}")
         product_list = driver.find_elements_by_xpath("//*[@class='a-link-normal a-text-normal']")
         page_asins = [i.get_attribute("href").split("/dp/")[1].split("/")[0] for i in product_list]
-        all_asins.append(page_asins)
+        all_asins += page_asins
     return all_asins
 
 
-if __name__== "__main__":
+def get_product_details(asin):
+    driver.get(f"{PROD_BASE_URL}{asin}")
+    title = driver.find_element_by_id("productTitle").text
+    return (asin, title)
+
+
+if __name__ == "__main__":
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROME_PATH", CHROMEDRIVER_PATH), chrome_options=options)
-    print(get_product_list("AK3PDFUAKQTEL", 3))
+    asins_list = get_product_list("AK3PDFUAKQTEL", NUM_PAGES)
+    for asin in asins_list:
+        print(get_product_details(asin))
